40c8da7d0d4bbaff2f933eddbb6a06c0f18a0ca5
Emir0zcelik <86933017+Emir0zcelik@users.noreply.github.com>
2022-01-05 18:00:07 +0300

    Create Doctor.h

---
+++ Doctor.h
@@ -0,0 +1,24 @@
+#ifndef _DOCTOR_H
+#define _DOCTOR_H
+#include "Person.h"
+
+class Doctor : public Person {
+private:
+	int doc_id, dep_id, doc_shift_start, doc_shift_end;
+
+public:
+	Doctor();
+
+	Doctor(int doc_id, int dep_id, int doc_shift_start, int doc_shift_end);
+	// Copy constructor
+	Doctor(const Doctor &doc1);
+	
+	void set_doc_id(int doc_id);
+	void set_dep_id(int dep_id);
+	int get_doc_id() const;
+	int get_dep_id() const;
+	int get_shift_start()const;
+	int get_shift_end()const;
+};
+
+#endif
